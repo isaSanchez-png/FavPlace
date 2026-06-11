af4087979e4b2dff586e0d3196c8cae4efa69b29
isaSanchez-png <isabela.sanchez.mol@gmail.com>
2026-06-11 16:03:28 -0500

    Initial Commit

---
+++ FavPlace - App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FavPlace - App
+//
+//  Created by Isa on 11/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
